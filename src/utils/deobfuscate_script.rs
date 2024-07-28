b87ed88ae4edeaf6c3bbf4bb6bbd6c62367c88e0
Peet <gh@peet.ws>
2024-07-28 16:18:27 +0200

    chore: share marks, use resolver & simplifier

--- src/utils/deobfuscate_script.rs
+++ src/utils/deobfuscate_script.rs
@@ -3,14 +3,17 @@ use crate::extractors::{config_builder::VMConfig, extractor};
 use crate::transformers;
 use swc_common::{chain, comments::SingleThreadedComments, Mark, GLOBALS};
 use swc_core::ecma::transforms;
-use swc_ecma_transforms::optimization::simplify::expr_simplifier;
+use swc_ecma_transforms::optimization::simplifier;
+use swc_ecma_transforms::optimization::simplify::{expr_simplifier, Config};
 use swc_ecma_transforms::pass::noop;
+use swc_ecma_transforms::resolver;
 use swc_ecma_visit::as_folder;
 
 pub fn deobfuscate(cnfg: &mut VMConfig, src: &str) -> String {
     let mut out: String = "".to_owned();
 
     GLOBALS.set(&Default::default(), || {
+        let marks = Mark::new();
         let data = utils::get_structs(src);
         let output = data
             .comp
@@ -23,21 +26,20 @@ pub fn deobfuscate(cnfg: &mut VMConfig, src: &str) -> String {
                 |_| noop(),
                 |_| {
                     chain!(
+                        resolver(marks, marks, false),
                         as_folder(transformers::strings::Visitor::new(src.to_string())),
                         as_folder(transformers::proxy_functions::Visitor),
                         as_folder(transformers::computed_members::Visitor),
                         as_folder(transformers::control_flow_flattening::Visitor),
                         as_folder(transformers::cleanup_deleted::Visitor),
                         as_folder(transformers::sequence_expressions::Visitor),
-                        expr_simplifier(Mark::new(), Default::default()),
-                        transforms::optimization::simplify::dce::dce(
-                            Default::default(),
-                            Mark::new(),
-                        ),
+                        expr_simplifier(marks, Default::default()),
+                        transforms::optimization::simplify::dce::dce(Default::default(), marks,),
                         as_folder(transformers::useless_if::Visitor),
                         as_folder(transformers::simplify_binary::Visitor),
                         // extractor: Only required for parsing the script, not deobfuscating it
                         as_folder(extractor::Visitor { cnfg: cnfg }),
+                        simplifier(marks, Config::default()),
                     )
                 },
             )
